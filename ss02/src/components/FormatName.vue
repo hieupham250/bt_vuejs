4ddd4a698baeb79240f2c356cbd07dbf8dc84fc6
Hiếu Phạm <ptrunghieu2507@gmail.com>
2024-09-19 07:35:02 +0700

    bai tap

---
+++ ss02/src/components/FormatName.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <b>Họ và tên: {{ fullName }}</b>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const user = {
+  firstName: "Phạm Trung",
+  lastName: "Hiếu",
+};
+
+const formatName = (user) => {
+  return `${user.firstName} ${user.lastName}`;
+};
+
+const fullName = ref(formatName(user));
+</script>
+
+<style></style>
---
+++ ss02/src/components/UserInfo.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h1>Thông tin cá nhân</h1>
+    <ul>
+      <li>
+        Họ và tên: <b>{{ personalInfo.fullName }}</b>
+      </li>
+      <li>
+        Giới tính: <b>{{ personalInfo.gender }}</b>
+      </li>
+      <li>
+        Ngày sinh: <b>{{ personalInfo.dob }}</b>
+      </li>
+      <li>
+        Email: <b>{{ personalInfo.email }}</b>
+      </li>
+      <li>
+        Địa chỉ: <b>{{ personalInfo.address }}</b>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+const personalInfo = {
+  fullName: "Phạm Trung Hiếu",
+  gender: "Nam",
+  dob: "25/07/2004",
+  email: "ptrunghieu2507@gmail.com",
+  address: "Thanh Xuân, Hà Nội",
+};
+</script>
+
+<style></style>
