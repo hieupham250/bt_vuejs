95740957aecb39a14b1305d8442cb40923615901
Hiếu Phạm <ptrunghieu2507@gmail.com>
2024-09-20 07:48:33 +0700

    bai tap

--- ss03/src/App.vue
+++ ss03/src/App.vue
@@ -9,6 +9,7 @@
     <B7 />
     <B8 />
     <B9 />
+    <B10 />
     <B11 />
     <B12 />
   </div>
@@ -24,6 +25,7 @@ import B6 from "./components/B6.vue";
 import B7 from "./components/B7.vue";
 import B8 from "./components/B8.vue";
 import B9 from "./components/B9.vue";
+import B10 from "./components/B10.vue";
 import B11 from "./components/B11.vue";
 import B12 from "./components/B12.vue";
 </script>
---
+++ ss03/src/components/B10.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <input
+      type="text"
+      v-model="inputText"
+      ref="inputField"
+      @keyup.enter="handleKeyUp"
+      placeholder="Nhập nội dung và nhấn Shift + Enter"
+    />
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+const inputText = ref("");
+const inputField = ref(null);
+
+const handleKeyUp = (event) => {
+  if (event.shiftKey && event.key === "Enter") {
+    if (inputText.value) {
+      alert(inputText.value);
+      inputText.value = "";
+    }
+  } else if (event.key === "Enter") {
+    alert("Bạn cần nhấn Shift + Enter");
+  }
+};
+</script>
+
+<style></style>
