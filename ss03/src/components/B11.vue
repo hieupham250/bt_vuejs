915250cc4490f85586018a6758d03cc4a4d77ad4
Hiếu Phạm <ptrunghieu2507@gmail.com>
2024-09-20 07:36:16 +0700

    bai tap

---
+++ ss03/src/components/B11.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <div :class="['box', isActive ? 'active' : '']"></div>
+    <button @click="handleChangeBg">Change background</button>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+const isActive = ref(false);
+
+const handleChangeBg = () => {
+  isActive.value = !isActive.value;
+};
+</script>
+
+<style>
+.box {
+  margin: 20px;
+  width: 100px;
+  height: 100px;
+  background-color: red;
+}
+
+.active {
+  background-color: blue;
+}
+</style>
---
+++ ss03/src/components/B12.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <img :src="url" alt="" style="width: 300px; height: 200px" />
+    <button @click="handleChangeImage">Change image</button>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+const urls = [
+  "https://png.pngtree.com/thumb_back/fh260/background/20230408/pngtree-powder-smoke-colorful-background-image_2164096.jpg",
+  "https://w0.peakpx.com/wallpaper/1004/268/HD-wallpaper-vector-art-flowers-background-jpg-neon-flowers-clear-blue-thumbnail.jpg",
+  "https://png.pngtree.com/thumb_back/fh260/background/20230408/pngtree-rainbow-curves-abstract-colorful-background-image_2164067.jpg",
+  "https://static.vecteezy.com/system/resources/thumbnails/023/669/545/small/abstract-gradient-pink-blue-liquid-wave-background-free-vector.jpg",
+];
+const url = ref(urls[0]);
+
+const handleChangeImage = () => {
+  const randomIndex = Math.floor(Math.random() * urls.length);
+  url.value = urls[randomIndex];
+};
+</script>
+
+<style></style>
