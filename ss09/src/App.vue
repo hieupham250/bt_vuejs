0a849c1629eb20def9ac5e7147999e9fb6a38376
Hiếu Phạm <ptrunghieu2507@gmail.com>
2024-10-02 07:52:17 +0700

    bai tap

--- ss09/src/App.vue
+++ ss09/src/App.vue
@@ -19,6 +19,7 @@ import B1 from "./components/B1.vue";
 import B2 from "./components/B2.vue";
 import B3 from "./components/B3.vue";
 import B4 from "./components/B4.vue";
+import B5 from "./components/B5.vue";
 import B6 from "./components/B6.vue";
 import B7 from "./components/B7.vue";
 import B8 from "./components/B8.vue";
